6a593093f76dcd99fd65b7abc96f2afa6701f29b
“aisse” <aisse.camara@gmail.com>
2023-09-25 15:37:19 +0200

    form avec email et password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+
 const defaultFormFields = {
     displayName: '',
     email: '',
@@ -14,6 +16,10 @@ const SignUpForm = () => {
     } = formFields;
 
     console.log(formFields);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    }
 
     const handleChange = (event) => {
         const {name, value} = event.target;
